3a006a081140605b4e35288bdb8fdbd132708a3b
JOSUA BRINK - STUDENT <G00362643@gmit.ie>
2022-03-09 10:26:07 +0000

    Test Add Employee

--- src/main/java/gmit/ie/EmployeeList.java
+++ src/main/java/gmit/ie/EmployeeList.java
@@ -1,4 +1,24 @@
 package gmit.ie;
 
+import java.util.ArrayList;
+
 public class EmployeeList {
+    private ArrayList<Employee> employeeList;
+
+    public EmployeeList() {
+        employeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee e) {
+        for(Employee i : employeeList) {
+            if(e.getPPS().equals(i.getPPS())) {
+                throw new IllegalArgumentException("Employee already exists in list");
+            }
+        }
+        employeeList.add(e);
+    }
+
+    public int getListSize() {
+        return employeeList.size();
+    }
 }
