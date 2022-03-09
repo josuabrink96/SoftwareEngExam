e3e1ba6ffca38fa371bc88b0451758884849458d
JOSUA BRINK - STUDENT <G00362643@gmit.ie>
2022-03-09 09:31:11 +0000

    Test Title

--- src/main/java/gmit/ie/Employee.java
+++ src/main/java/gmit/ie/Employee.java
@@ -1,4 +1,23 @@
 package gmit.ie;
 
 public class Employee {
+    private String title;
+
+    public Employee(String title)
+    {
+        setTitle(title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if(title.equals("Mr") || title.equals("Mrs") || title.equals("Miss")) {
+            this.title = title;
+        }
+        else {
+            throw new IllegalArgumentException("Invalid title entered");
+        }
+    }
 }
--- src/test/java/gmit/ie/EmployeeTest.java
+++ src/test/java/gmit/ie/EmployeeTest.java
@@ -1,4 +1,26 @@
 package gmit.ie;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EmployeeTest {
+    @Test
+    void testTitle()
+    {
+        String expectedTitle = "Mr";
+        Employee e = new Employee("Mr");
+        assertEquals(expectedTitle, e.getTitle());
+    }
+
+    @Test
+    void testInvalidTitle()
+    {
+        String expectedMessage = "Invalid title entered";
+        Exception e = assertThrows(
+                IllegalArgumentException.class,
+                ()-> new Employee("Invalid")
+        );
+        assertEquals(expectedMessage, e.getMessage());
+    }
+
 }
